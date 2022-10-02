5d4aa2e674f439387cae32aa8a38ca0bf243dfe2
Porkius <114440254+Porkius@users.noreply.github.com>
2022-10-02 14:55:08 -0300

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use bevy::prelude::*;
+
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+    .insert_resource(WindowDescriptor {
+        width: 640.0,
+        height: 360.0,
+        title: "Minesweeper Showdown".to_string(),
+        resizable: true,
+        ..Default::default()
+    })
+    .add_plugins(DefaultPlugins)
+    .run();
 }
