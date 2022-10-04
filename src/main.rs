a18879eb8ddd7166a1d186bec8691546e310acf6
Benjamin Farquhar <bennylew2@gmail.com>
2022-10-04 14:12:28 -0300

    - Changed import statment to be specific. - Extracted height,width to consts.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
-use bevy::prelude::*;
+use bevy::prelude;
 
+const WIDTH: f32 = 640.0;
+const HEIGHT: f32 = 360.0;
 
 fn main() {
-    App::new()
-    .insert_resource(WindowDescriptor {
-        width: 640.0,
-        height: 360.0,
+    prelude::App::new()
+    .insert_resource(prelude::WindowDescriptor {
+        width: WIDTH, 
+        height: HEIGHT,
         title: "Minesweeper Showdown".to_string(),
         resizable: true,
         ..Default::default()
     })
-    .add_plugins(DefaultPlugins)
+    .add_plugins(prelude::DefaultPlugins)
     .run();
 }
